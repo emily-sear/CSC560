e4e0e5da930a1f73f4274cd64d51f3e75c1d1961
Emily Sear <emily.sear@cuw.edu>
2023-03-02 20:05:58 -0600

    Get commands set up with UI

--- Unit 4/Assignment4.3/my-app/src/app/app.component.ts
+++ Unit 4/Assignment4.3/my-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
-}
+  title = 'Assignment 4.3';
+  constructor(private http: HttpClient) {}
+  allCrochetPatterns = ""
+  singleCrochetPattern = ""
+  getAllCrochetPatterns() {
+
+    this.http.get('http://localhost:3000/api/getAll').subscribe(data => {
+      console.log(data);
+      this.allCrochetPatterns ="";
+      for(let i = 0; i < Object.keys(data).length; i++) {
+        if(i != 0) {
+          this.allCrochetPatterns += ", " + Object.values(data)[i].name;
+        } else {
+          this.allCrochetPatterns += Object.values(data)[i].name;
+        }
+
+      }
+    })
+  }
+
+  getSingleCrochetPattern() {
+    let inputValue = (<HTMLInputElement>document.getElementById("idInput")).value
+    if(inputValue !== "") {
+      this.http.get('http://localhost:3000/api/getOne/' + inputValue).subscribe(data => {
+        console.log(data);
+        this.singleCrochetPattern = "Name: " + Object.values(data)[1] + "\n";
+        this.singleCrochetPattern += "Yarn Weight: " + Object.values(data)[2] + "\n";
+        this.singleCrochetPattern += "Hook size: " + Object.values(data)[3] + "\n";
+        this.singleCrochetPattern += "Amount of yarn: " + Object.values(data)[4] +"\n";
+    })
+    }
+    }
+  }
